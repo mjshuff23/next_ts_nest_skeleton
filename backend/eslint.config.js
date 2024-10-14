f6d237dda21d1a7317e75319d300f15936f5bf86
Michael Shuff <mjshuff23@gmail.com>
2024-10-13 21:54:55 -0400

    feat: re-add backend folder as a regular directory

---
+++ backend/eslint.config.js
@@ -0,0 +1,12 @@
+import { baseConfig } from '../eslint.config.js';
+
+export default {
+  ...baseConfig,
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+  ignores: ['.eslintrc.json'],
+};
